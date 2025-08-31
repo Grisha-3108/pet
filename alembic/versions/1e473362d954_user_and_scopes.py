86213f1d51ed0f8b204f7a611aca4e0bd15e9af4
Tikhanovsky Grigory <Grisha-3108@yandex.ru>
2025-08-31 13:07:17 +0300

    Admin user migration added with all scopes

--- alembic/versions/1e473362d954_user_and_scopes.py
+++ alembic/versions/1e473362d954_user_and_scopes.py
@@ -1,18 +1,22 @@
-"""User and scope
+"""User and scopes
 
-Revision ID: 893f1dc33f82
+Revision ID: 1e473362d954
 Revises: 
-Create Date: 2025-08-30 19:57:53.705589
+Create Date: 2025-08-31 12:38:32.832844
 
 """
 from typing import Sequence, Union
+import uuid
 
 from alembic import op
 import sqlalchemy as sa
 
+from models.user_scope import Scope
+from models import User, UserScope
+from authorization.hashing import hash_password
 
 # revision identifiers, used by Alembic.
-revision: str = '893f1dc33f82'
+revision: str = '1e473362d954'
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -25,7 +29,7 @@ def upgrade() -> None:
     sa.Column('username', sa.String(length=100), nullable=False),
     sa.Column('hashed_password', sa.String(length=60), nullable=False),
     sa.Column('is_active', sa.Boolean(), server_default='True', nullable=False),
-    sa.Column('is_verified', sa.Boolean(), server_default='True', nullable=False),
+    sa.Column('is_verified', sa.Boolean(), server_default='False', nullable=False),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('username')
     )
@@ -35,6 +39,16 @@ def upgrade() -> None:
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], name='user_id_fk', ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('user_id', 'scope', name='user_scopes_pk')
     )
+
+    user_id = uuid.uuid4()
+    op.bulk_insert(User.__table__, [{'id': user_id, 
+                             'username': 'Grisha-3108@yandex.ru',
+                             'hashed_password': hash_password('11223344'),
+                             'is_active': True,
+                             'is_verified': True}])
+    op.bulk_insert(UserScope.__table__, [{'user_id': user_id, 'scope': Scope.modify_scope.name},
+                   {'user_id': str(user_id), 'scope': Scope.delete.name},
+                   {'user_id': str(user_id), 'scope': Scope.update.name}])
 
 
 def downgrade() -> None:
